d0c8149132dc1e03561d1be20fa7fe0361fa9788
Chris Troutner <chris.troutner@gmail.com>
2021-05-14 13:46:12 -0700

    Created an initial JS app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+  The first file that gets executed, starts the REST API app.
+*/
+
+setInterval(function() {
+  const now = new Date();
+
+  console.log(`Timestamp: ${now.toLocaleString()}`);
+}, 5000);
